41fa8051d86b2ee014aeb61e3bf59e2271c89b21
Peter Klipfel <peter@klipfel.me>
2014-04-04 15:25:58 -0600

    almost compiling

--- get_input.hs
+++ get_input.hs
@@ -1,22 +1,34 @@
 import Control.Monad  
 import Data.Char
 import Data.Binary as B
-import Data.Binary.Get
-import qualified Data.ByteString as B
+import qualified Data.Binary.Get as BG
+import qualified Data.ByteString as BS
 import qualified Data.ByteString.Lazy as BL
+import Data.Binary.Put
+import qualified Data.Binary.Bits.Get as BBG
 
 import System.IO
   
 main = forever $ do
   contents <- BL.getContents
-  print $ runGet getMidi contents
+  print $ BG.runGet getMidi contents
 
 data Midi = Midi
-  {
-    bytes :: !Word16
+  { first  :: !(Word8, Word8)
+  , second :: !(Word8, Word8)
   } deriving (Show)
+
 
 getMidi :: Get Midi
 getMidi = do
-  bytes <- getWord16le
-  return $! Midi bytes
+  bytes  <- BG.getWord16le
+  bytes2 <- BG.getWord16le
+  let blah1 = parse4by4 bytes
+  let blah2 = parse4by4 bytes2
+  return $! Midi blah1 blah2
+
+parse4by4 :: BBG.BitGet (Word8, Word8)
+parse4by4 = do
+   bits <- BBG.getWord8 4
+   more <- BBG.getWord8 4
+   return (bits,more)
