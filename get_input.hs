c5873de3fc6dd7d3c785b57a105227d478131af7
Peter Klipfel <peter@klipfel.me>
2014-04-08 17:37:36 -0600

    grabbing bits

--- get_input.hs
+++ get_input.hs
@@ -7,28 +7,30 @@ import qualified Data.ByteString.Lazy as BL
 import Data.Binary.Put
 import qualified Data.Binary.Bits.Get as BBG
 
+import Control.Applicative
+
 import System.IO
   
 main = forever $ do
   contents <- BL.getContents
-  print $ BG.runGet getMidi contents
+  let bits = BG.runGet (BBG.runBitGet parse4by4) contents
+  print bits
 
-data Midi = Midi
-  { first  :: !(Word8, Word8)
-  , second :: !(Word8, Word8)
-  } deriving (Show)
+--data Midi = Midi
+--  { first  :: [Word8]
+--  , second :: [Word8]
+--  } deriving (Show)
 
 
-getMidi :: Get Midi
-getMidi = do
-  bytes  <- BG.getWord16le
-  bytes2 <- BG.getWord16le
-  let blah1 = parse4by4 bytes
-  let blah2 = parse4by4 bytes2
-  return $! Midi blah1 blah2
+--getMidi :: Get Midi
+--getMidi = do
+--  first  <- BBG.getWord8
+--  second <- BBG.getWord8
+--  return $! Midi first second
 
-parse4by4 :: BBG.BitGet (Word8, Word8)
-parse4by4 = do
-   bits <- BBG.getWord8 4
-   more <- BBG.getWord8 4
-   return (bits,more)
+parse4by4 = 
+  (,,,)
+    <$> BBG.getWord32be 8
+    <*> BBG.getWord32be 8
+    <*> BBG.getWord32be 8
+    <*> BBG.getWord32be 8
