53378793aec53bda5e14ee727f34c73cceb4b35f
Peter Klipfel <peter@klipfel.me>
2014-04-27 15:06:19 -0600

    specifying midi get

--- get_input.hs
+++ get_input.hs
@@ -7,13 +7,15 @@ import qualified Data.ByteString.Lazy as BL
 import Data.Binary.Put
 import qualified Data.Binary.Bits.Get as BBG
 
+import Test.Robot
+
 import Control.Applicative
 
 import System.IO
   
 main = forever $ do
   contents <- BL.getContents
-  let bits = BG.runGet (BBG.runBitGet parse4by4) contents
+  let bits = BG.runGet (BBG.runBitGet parseMidi) contents
   print bits
   printOrNot $ sortMidi bits
 
@@ -22,24 +24,13 @@ printOrNot (Just n) = print n
 printOrNot Nothing = return ()
 
 sortMidi :: (Integral a) => (a, a, a) -> Maybe(String)
-sortMidi (a, 48, c) = Just("a") 
-sortMidi (a, 49, c) = Just("b") 
-sortMidi (a, 50, c) = Just("c") 
+sortMidi (a, 48, c) = Just("a")
+sortMidi (a, 49, c) = Just("b")
+sortMidi (a, 50, c) = Just("c")
 sortMidi x = Nothing
 
---data Midi = Midi
---  { first  :: [Word8]
---  , second :: [Word8]
---  } deriving (Show)
 
-
---getMidi :: Get Midi
---getMidi = do
---  first  <- BBG.getWord8
---  second <- BBG.getWord8
---  return $! Midi first second
-
-parse4by4 = 
+parseMidi = 
   (,,)
     <$> BBG.getWord8 8
     <*> BBG.getWord8 8
