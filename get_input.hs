5c9e9585bbb5e42577f525461575c6ac7ec5eb94
Peter Klipfel <peter@klipfel.me>
2014-04-28 21:09:20 -0600

    output values based on midi signal

--- get_input.hs
+++ get_input.hs
@@ -1,34 +1,24 @@
 import Control.Monad  
-import Data.Char
-import Data.Binary as B
-import qualified Data.Binary.Get as BG
-import qualified Data.ByteString as BS
-import qualified Data.ByteString.Lazy as BL
+import Control.Applicative
 import Data.Binary.Put
+import System.IO
+import Test.Robot
+import qualified Data.Binary.Get as BG
+import qualified Data.ByteString.Lazy as BL
 import qualified Data.Binary.Bits.Get as BBG
 
-import Test.Robot
-
-import Control.Applicative
-
-import System.IO
   
 main = forever $ do
-  contents <- BL.getContents
-  let bits = BG.runGet (BBG.runBitGet parseMidi) contents
-  print bits
-  printOrNot $ sortMidi bits
+  bits <- fmap getbits $ BL.getContents 
+  runRobot $ pressKey bits
+    where 
+      getbits = BG.runGet $ BBG.runBitGet parseMidi
 
-printOrNot :: Maybe(String) -> IO ()
-printOrNot (Just n) = print n
-printOrNot Nothing = return ()
-
-sortMidi :: (Integral a) => (a, a, a) -> Maybe(String)
-sortMidi (a, 48, c) = Just("a")
-sortMidi (a, 49, c) = Just("b")
-sortMidi (a, 50, c) = Just("c")
-sortMidi x = Nothing
-
+pressKey :: (Integral a) => (a, a, a) -> Robot()
+pressKey (a, 48, c) = tap _Tilde
+pressKey (a, 49, c) = tap _Space
+pressKey (a, 50, c) = tap _Tilde
+pressKey x = tap _Space
 
 parseMidi = 
   (,,)
