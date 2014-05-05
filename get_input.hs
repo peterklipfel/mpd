5d0f06e5660c37c2da0a8282be69e614646130cf
Peter Klipfel <peter@klipfel.me>
2014-05-05 15:09:11 -0600

    only executing keystroke when midi is recognized

--- get_input.hs
+++ get_input.hs
@@ -10,15 +10,19 @@ import qualified Data.Binary.Bits.Get as BBG
   
 main = forever $ do
   bits <- fmap getbits $ BL.getContents 
-  runRobot $ pressKey bits
+  getKey bits
     where 
       getbits = BG.runGet $ BBG.runBitGet parseMidi
 
-pressKey :: (Integral a) => (a, a, a) -> Robot()
-pressKey (a, 48, c) = tap _Tilde
-pressKey (a, 49, c) = tap _Space
-pressKey (a, 50, c) = tap _Tilde
-pressKey x = tap _Space
+getKey :: (Integral a) => (a, a, a) -> IO()
+getKey (a, 48, c) = runRobot $ tap _Tilde
+getKey (a, 49, c) = runRobot $ tap _Space
+getKey (a, 50, c) = runRobot $ tap _Tilde
+getKey x = return ()
+
+--pressKey :: Maybe(Robot()) -> IO()
+--pressKey Just x  = runRobot x
+--pressKey Nothing = ()
 
 parseMidi = 
   (,,)
